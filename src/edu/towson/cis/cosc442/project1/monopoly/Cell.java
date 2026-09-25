ae27e6340e33b05b155342b065d8e6b38626655d
NyleClem <nyleclementsstem@outlook.com>
2026-09-25 14:23:03 -0400

    Task 9 - Rename field owner to theOwner in Cell.java + add javadoc comments to Cell.java

--- src/edu/towson/cis/cosc442/project1/monopoly/Cell.java
+++ src/edu/towson/cis/cosc442/project1/monopoly/Cell.java
@@ -3,38 +3,73 @@ package edu.towson.cis.cosc442.project1.monopoly;
 public abstract class Cell {
 	private boolean available = true;
 	private String name;
-	protected Player owner;
+	protected Player theOwner;
 
+	/**
+	 * Returns the name of the cell.
+	 * @return The name of the cell.
+	 */
 	public String getName() {
 		return name;
 	}
 
-	public Player getOwner() {
-		return owner;
+	/**
+	 * Returns the player who owns this cell.
+	 * @return The owner of the cell as a Player object, or null if unowned.
+	 */
+	public Player getTheOwner() {
+		return theOwner;
 	}
 	
+	/**
+	 * Returns the price of the cell, defaulting to 0.
+	 * @return The price of the cell as an integer.
+	 */
 	public int getPrice() {
 		return 0;
 	}
 
+	/**
+	 * Checks if the cell is currently available.
+	 * @return true if the cell is available; false otherwise.
+	 */
 	public boolean isAvailable() {
 		return available;
 	}
 	
+	/**
+	 * Performs the specific action associated with landing on this cell.
+	 */
 	public abstract void playAction();
 
+	/**
+	 * Sets whether the cell is available.
+	 * @param available Indicates the availability status to set.
+	 */
 	public void setAvailable(boolean available) {
 		this.available = available;
 	}
 	
+	/**
+	 * Sets the name of the cell.
+	 * @param name The new name to assign to the cell.
+	 */
 	void setName(String name) {
 		this.name = name;
 	}
 
-	public void setOwner(Player owner) {
-		this.owner = owner;
+	/**
+	 * Assigns the owner of the cell.
+	 * @param owner The Player to set as the owner of the cell.
+	 */
+	public void setTheOwner(Player owner) {
+		this.theOwner = owner;
 	}
     
+    /**
+     * Returns a string representation of the cell.
+     * @return The name of the cell as a string.
+     */
     public String toString() {
         return name;
     }
